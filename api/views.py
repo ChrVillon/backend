2508c79dc461fbb775e195db99d3c4f24c4201c6
chrVillon <chrovill@espol.edu.ec>
2025-01-08 20:28:46 -0500

    se agregaron metodos para la tarea

--- api/views.py
+++ api/views.py
@@ -42,3 +42,43 @@ class LandingAPI(APIView):
 	        
         # Devuelve el id del objeto guardado
         return Response({"id": new_resource.key}, status=status.HTTP_201_CREATED)
+    
+# Tarea     
+class LandingAPIDetail(APIView):
+
+    name = 'Landing Detail API'
+
+    collection_name = 'data'
+
+    def get(self, request, pk):
+        ref = db.reference(f'{self.collection_name}/{pk}')
+        data = ref.get()
+        
+        if data:
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Documento no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, pk):
+        required_fields = ["consulta", "email", "nombre"]
+
+        for field in required_fields:
+            if field not in request.data:
+                return Response({"error": f"El campo '{field}' es obligatorio"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        ref = db.reference(f'{self.collection_name}/{pk}')
+        data = ref.get()
+        
+        if not data:
+            return Response({"error": "Documento no encontrado"}, status=status.HTTP_404_NOT_FOUND)    
+        
+        ref.update(request.data)
+        return Response({"message": "Documento actualizado exitosamente"}, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        ref = db.reference(f'{self.collection_name}/{pk}')
+        if ref.get() is not None:
+            ref.delete()
+            return Response({"message": "Documento eliminado correctamente."}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({"error": "Documento no encontrado."}, status=status.HTTP_404_NOT_FOUND)            
